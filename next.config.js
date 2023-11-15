778c6d93fc422a4f39b1e5e274ec8db670484f03
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-15 11:06:46 +0500

    next-config-mod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+  },
+};
