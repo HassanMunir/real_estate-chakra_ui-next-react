99ae511b5af625f99bdcb1e92b6faa129e15e217
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-15 18:20:47 +0500

    index-mod

--- pages/index.js
+++ pages/index.js
@@ -63,6 +63,7 @@ export default function Home() {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
+      {/* fetch the properties and map over them...  */}
     </Box>
   );
 }
